972461d2e26497df9ceb6996b11265f8df5c150f
Michael Antoni <michaelantoni.tech@gmail.com>
2026-08-16 01:14:50 +0800

    update ai model into openai/gpt-oss-120b

--- backend/src/s3-queue-consumer/index.mjs
+++ backend/src/s3-queue-consumer/index.mjs
@@ -252,7 +252,7 @@ const useAIToAnalyzeText = async (payload) => {
   try {
     // AI parameters
     const chatCompletion = await groq.chat.completions.create({
-      model: 'llama-3.3-70b-versatile',
+      model: 'openai/gpt-oss-120b',
       messages: [
         {
           role: 'system',
